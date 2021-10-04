c63a3a54bca998fa003532c9fad8815b92a79f56
Ethan McComb <ethamcco802@ed.amdsb.ca>
2021-10-04 11:05:26 -0400

    Add project files.

---
+++ Frogger/Logs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Frogger
+{
+    class Logs
+    {
+        public int x, y, size, speed;
+        public SolidBrush brushColor;
+
+        public Logs(int _x, int _y, int _size, int _speed, SolidBrush _brushColor)
+        {
+            x = _x;
+            y = _y;
+            size = _size;
+            speed = _speed;
+            brushColor = _brushColor;
+        }
+        public void Move()
+        {
+            y += speed;
+        }
+        public void Move(string direction)
+        {
+            y += speed;
+
+        }
+        
+    }
+}
---
+++ Frogger/MainScreen.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frogger
+{
+    public partial class MainScreen : UserControl
+    {
+        Boolean upArrowDown, downArrowDown;
+
+        List<Logs> Log = new List<Logs>();
+
+        int xLeft = 250;
+        int gap = 300;
+
+        bool moveRight = true;
+        int patternLength = 10;
+        int xChange = 20;
+
+        int newBoxCounter = 0;
+
+        Logs hero;
+
+        Random randGen = new Random();
+
+        public MainScreen()
+        {
+            InitializeComponent();
+            OnStart();
+        }
+
+        public void OnStart()
+        {
+
+        }
+
+        private void MainScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+
+            }
+        }
+
+        public void CreateLog()
+        {
+
+        }
+
+
+    }
+}
